c67761a4807db65b6a2295b986695dc93d8ca1e7
Dilshod <ddilshod2197@gmail.com>
2026-04-17 15:05:56 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+ism = input("Ismingiz: ")
+yosh = int(input("Yoshingiz: "))
+
+if yosh < 18:
+    print(f"Salom, {ism}! Siz bolasiz 😊")
+elif yosh < 30:
+    print(f"Assalom, {ism}! Yosh va faolsiz! 🚀")
+else:
+    print(f"Salom, {ism}! Tajribalisiz 💪")
